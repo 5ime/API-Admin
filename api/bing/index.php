942f091b1a5149d2452d1250d8345e9c164985e5
5ime <suozhang233@gmail.com>
2020-04-23 09:12:34 +0800

    新增6个API

---
+++ api/bing/index.php
@@ -0,0 +1,15 @@
+<?php
+ $str=file_get_contents('http://cn.bing.com/HPImageArchive.aspx?idx=0&n=1');
+ if(preg_match("/<url>(.+?)<\/url>/ies",$str,$matches)){
+  $imgurl='http://cn.bing.com'.$matches[1];
+ }
+ if($imgurl){
+  header('Content-Type: image/JPEG');
+  @ob_end_clean();
+  @readfile($imgurl);
+  @flush(); @ob_flush();
+  exit();
+ }else{
+  exit('error');
+ }
+?>
