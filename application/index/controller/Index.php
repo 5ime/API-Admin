c041b6527018fb11f2a943db137c8e9bc57b5378
I Am I <31686695+5ime@users.noreply.github.com>
2020-04-23 09:39:59 +0800

    美化部分代码

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -8,11 +8,8 @@ use app\index\model\Cate as CateModel;
 class Index extends Controller
 {
     public function index(){
-        //信息查询
         $info=Db::name('setup')->find();
-        //API查询
         $api=Db::name('info')->select();
-        //API数量
         $apicount=Db::name('info')->count();
         return view('index', [
             'info' => $info,
@@ -20,7 +17,6 @@ class Index extends Controller
             'count' => $apicount,
         ]);  
     }
-
     public function doc(){
     	$doc=input('id');
         $api=Db::name('info')->where("doc='$doc'")->find();
@@ -31,14 +27,10 @@ class Index extends Controller
             header("HTTP/1.0 404 Not Found"); 
             return $this->fetch(APP_PATH.'404.html');
         }
-        
     	return view('doc',[
             'api'	=> $api,
             'info' => $info,
             'list' => $list,
     	]);
 	}
-
-
-
 } 
