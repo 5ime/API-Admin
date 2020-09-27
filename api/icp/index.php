a4d2b090b4ee5b67355063e48d97033bfccab0b8
I Am I <31686695+5ime@users.noreply.github.com>
2020-09-27 13:42:04 +0800

    修复icp备案查询获取失败

--- api/icp/index.php
+++ api/icp/index.php
@@ -1,53 +1,44 @@
 <?php
+/**
+ * @package 备案查询
+ * @author 5ime
+ * @version 1.0.0
+ * @link http://github.com/5ime/api-admin/api/icp
+ */
 header("Access-Control-Allow-Origin:*");
 header('Content-type: application/json');
-$res = @$_GET['url'];
-if(strstr($res,"."))
-{
-$json = file_get_contents('http://icp.chinaz.com/'.$res); //调用的站长工具
-function GetBetween($content,$start,$end){
-$r = explode($start, $content);
-if (isset($r[1])){
-$r = explode($end, $r[1]);
-return $r[0];
-}
-return '';
-}
-// 获取主办单位名称
-$b ='主办单位名称</span><p>';
-$c ='<';
-$name = GetBetween($json,$b,$c);
-// 获取性质
-$b ='主办单位性质</span><p><strong class="fl fwnone">';
-$c ='<';
-$nature = GetBetween($json,$b,$c);
-// 获取备案号
-$b ='网站备案/许可证号</span><p><font>';
-$c ='<';
-$icp = GetBetween($json,$b,$c);
-// 获取网站名称
-$b ='网站名称</span><p>';
-$c ='<';
-$sitename = GetBetween($json,$b,$c);
-// 获取网站首页地址
-$b ='网站首页网址</span><p class="Wzno">';
-$c ='<';
-$index = GetBetween($json,$b,$c);
-if(strstr($index,"."))
-{ }else{
-echo '{"code":201,"msg":"未有此域名ICP备案记录!"}';
-exit();
-}
-// 获取审核时间
-$b ='审核时间</span><p>';
-$c ='<';
-$time = GetBetween($json,$b,$c);
-echo '{"code":"200","主办单位名称":"'. $name.'","主办单位性质":"'. $nature.'","网站备案/许可证号":"'. $icp.'","网站名称":"'. $sitename.'","网站首页网址":"'. $index.'","审核时间":"'. $time.'"}
-';
+$url = @$_GET['url'];
+$json = file_get_contents('http://icp.chinaz.com/info?q='.$url); //调用的站长工具
+//主办单位名称
+preg_match_all('/<td width=\"29%\" align=\"left\" class=\"by1\">(.*?)<\/td>/', $json, $mingcheng);
+//主办单位性质
+preg_match_all('/<td align=\"left\" class=\"by2\">(.*?)<\/td>/', $json, $xingzhi);
+//网站备案/许可证号 网站名称
+preg_match_all('/<td align=\"left\" class=\"by1\" width=\"30%\">(.*?)<\/td>/', $json, $beianhao);
+//网站首页
+preg_match_all('/<td align=\"left\" class=\"by2\" width=\"30%\" id=\"mpage\">(.*?)<\/td>/', $json, $index);
+//审核时间
+preg_match_all('/<td align=\"left\" class=\"by2\" width=\"30%\">(.*?)<\/td>/', $json, $time);
+//var_dump($index);
+if(strpos($beianhao[1][1],'--') !== false){
+    $Json = array(
+        'code' => 202,
+        'msg' => '未查询到备案信息',
+    );
 }else{
-echo '{"code":202,"msg":"域名不能为空!"}';
-// echo "内容不为空";
-// return false;
-exit();
+$Json = array(
+        "code" => 200,
+        "data" => array(
+            "name" => $mingcheng[1][0],
+            "nature" => $xingzhi[1][0],
+            "icp" => $beianhao[1][0],
+            "sitename" => $beianhao[1][1],
+            "index" => $index[1][0],
+            "time" => $time[1][0],
+        )
+    );
 }
+$Json = json_encode($Json,JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
+echo stripslashes($Json);
+return $Json;
 ?>
