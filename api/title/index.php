11a7ca89ae889bd5b266bfc32053d339af70b1e3
I Am I <31686695+5ime@users.noreply.github.com>
2020-09-27 13:48:23 +0800

    修复网站标题描述关键字获取失败

--- api/title/index.php
+++ api/title/index.php
@@ -1,19 +1,28 @@
 <?php
-if ($_GET['url']) {
-    $site='http://';
-    $url=trim($site.$_GET['url']);
-    $info=file_get_contents($url);
-    header('Content-type:text/json');
-    preg_match('|<title>(.*?)<\/title>|i',$info,$m);
-    if($url){ 
-      $result=array("code"=>"200","title"=>$m[1]);
-      echo json_encode($result,JSON_UNESCAPED_UNICODE);
+header('Access-Control-Allow-Origin:*');
+header('Content-type: application/json');
+$url = @$_GET["url"];
+$info=@file_get_contents($url);
+//var_dump($info);
+preg_match('/<title>(.*?)<\/title>/',$info,$title);
+preg_match('/name=\"description\" content=\"(.*?)\"/',$info,$description);
+preg_match('/name=\"keywords\" content=\"(.*?)\"/',$info,$keywords);
+if($title == NULL){
+    $Json = array(
+        "code"=> 202,
+        'msg' => '获取失败'
+    );
 }else{
-      $result=array("code"=>"201","meg"=>"ERREO");
-      echo json_encode($result,JSON_UNESCAPED_UNICODE);
+    $Json = array(
+        "code"=> 200,
+        'data' => array(
+            "title"=>@$title[1],
+            "description"=>@$description[1],
+            "keywords"=>@$keywords[1],
+        )
+    );
 }
-}else{
-      $result=array("code"=>"201","meg"=>"ERREO");
-      echo json_encode($result,JSON_UNESCAPED_UNICODE);
-}
+$Json = json_encode($Json,JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
+echo stripslashes($Json);
+return $Json;
 ?>
