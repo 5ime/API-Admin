ab75f054038b19fd85f1af2bfad9074f44e1ceb8
I Am I <31686695+5ime@users.noreply.github.com>
2022-01-07 16:59:42 +0800

    Update

--- config/app.php
+++ config/app.php
@@ -136,7 +136,7 @@ return [
     'dispatch_error_tmpl'    => Env::get('think_path') . 'tpl/dispatch_jump.tpl',
 
     // 异常页面的模板文件
-    'exception_tmpl'         => Env::get('think_path') . 'tpl/think_exception.tpl',
+    'exception_tmpl'         => 'application/404.html',
 
     // 错误显示信息,非调试模式有效
     'error_message'          => '页面错误！请稍后再试～',
