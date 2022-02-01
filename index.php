95a7298d7d47da6611817472420a49190897b80b
I Am I <31686695+5ime@users.noreply.github.com>
2022-02-01 16:30:34 +0800

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ header("Access-Control-Allow-Methods:GET, POST, OPTIONS, DELETE");
 header("Access-Control-Allow-Headers:DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type, Accept-Language, Origin, Accept-Encoding");
 
 // 加载基础文件
-require __DIR__ . './thinkphp/base.php';
+require __DIR__ . '/thinkphp/base.php';
 
 // 支持事先使用静态方法设置Request对象和Config对象
 
